f71082ba594882bff0d559929f24601daa008be7
Nic Glazkov <nic@glazkov.com>
2022-01-01 23:03:36 +0000

    Updated for automatic backups

--- backup.sh
+++ backup.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+mkdir -p /opt/minecraft/backups
+cd /opt/minecraft/backups
 tar -C /opt/minecraft/paper -czf $(date +"%Y-%m-%d").tar.gz  world world_nether world_the_end
 rm -f  $(date --date='7 days ago' +"%Y-%m-%d").tar.gz
 
