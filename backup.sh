eeed9d735b30184e4cfbd1d82b24b9e26ce692eb
Nic Glazkov <82780940+nicglazkov@users.noreply.github.com>
2022-02-05 18:25:28 -0800

    Changed backup removal period to 5 days

--- backup.sh
+++ backup.sh
@@ -2,4 +2,4 @@
 mkdir -p /opt/minecraft/backups
 cd /opt/minecraft/backups
 tar -C /opt/minecraft/paper -czf $(date +"%Y-%m-%d").tar.gz  world world_nether world_the_end
-rm -f /opt/minecraft/backups/$(date --date='7 days ago' +"%Y-%m-%d").tar.gz
+rm -f /opt/minecraft/backups/$(date --date='5 days ago' +"%Y-%m-%d").tar.gz
